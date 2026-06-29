569f66480971a872845787c40dc2b133b5013ca8
Cλstor <ygorcastor@gmail.com>
2026-06-29 18:03:04 +0200

    feat(equipment): send equip/unequip requests

--- game-engine/src/domain/equipment/mod.rs
+++ game-engine/src/domain/equipment/mod.rs
@@ -1,5 +1,7 @@
 pub mod location;
 pub mod plugin;
+pub mod request;
 
 pub use location::decode_wear_location;
 pub use plugin::EquipmentPlugin;
+pub use request::{EquipItemRequested, UnequipItemRequested};
